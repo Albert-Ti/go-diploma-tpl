b24d6565917d6ffe39afec819a82d3b3e9d518c3
Albert <albert@MacBook-Pro-Albert.local>
2026-06-15 13:13:29 +0300

    fix: log error GetBalanse

--- internal/handler/balance/get_balance.go
+++ internal/handler/balance/get_balance.go
@@ -2,6 +2,7 @@ package balance
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 
 	"github.com/Albert-Ti/go-diploma-tpl/internal/handler/auth"
@@ -18,6 +19,7 @@ func GetBalance(svc *service.Service) http.HandlerFunc {
 
 		balance, err := svc.GetBalance(r.Context(), userID)
 		if err != nil {
+			slog.Error("GetBalance", "ERROR", err)
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
